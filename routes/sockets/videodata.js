73399e61d7e18172994dc7481319e376c0188663
gshemdoe <georgehmariki@gmail.com>
2025-01-16 03:57:07 +0300

    updated

--- routes/sockets/videodata.js
+++ routes/sockets/videodata.js
@@ -2,6 +2,7 @@ const otpGen = require('otp-generator')
 const { uploadingTrailer, uploadingVideos } = require("../functions/download");
 
 const videoDataSocket = async (socket) => {
+
     const OTPName = otpGen.generate(32, {
         upperCaseAlphabets: false,
         specialChars: false,
@@ -10,18 +11,15 @@ const videoDataSocket = async (socket) => {
     });
 
     socket.on('videoInput', async (data) => {
-        let date = data.date
-        let video = data.video
-        let trailer = data.trailer
-        let caption = data.caption
-        let brand = data.brand
+        let {date, html_date, video, trailer, caption, brand} = data
+        let fname = `${html_date}-${OTPName}`
 
         //uploading trailer
-        await uploadingTrailer(socket, trailer, OTPName, OTPName, 'Trailer')
+        await uploadingTrailer(socket, trailer, fname, fname, 'Trailer')
         .catch(e => console.log(e))
 
         //uploading video
-        await uploadingVideos(socket, video, OTPName, 'Video File')
+        await uploadingVideos(socket, video, fname, 'Video File')
     });
 }
 
