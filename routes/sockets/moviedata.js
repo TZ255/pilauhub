a3e8ad81773ed66b9f8db81be6e93121965b2eb5
gshemdoe <georgehmariki@gmail.com>
2025-01-20 05:28:54 +0300

    done

--- routes/sockets/moviedata.js
+++ routes/sockets/moviedata.js
@@ -15,7 +15,7 @@ const movieDataSocket = (socket) => {
                 lowerCaseAlphabets: true
             });
 
-            const { html_date, nkiri_link } = data
+            const { nkiri_link } = data
             const nano = `${OTPName}`
 
             //scrape nkiri, getting download link, image and synopsis
@@ -41,7 +41,7 @@ const movieDataSocket = (socket) => {
             //downloading the file
             let botlink = `<a href="https://t.me/muvikabot?start=MOVIE-FILE${nano}">https://t.me/download/movie/${nano}</a>`
             const fileCaption = `<b>🎬 ${movieName} with English Subtitles</b>`
-            const photoCaption = `<b>🎬 Movie: ${movieName}</b>\n\n\n<b>📄 Overview:</b>\n${synopsisText}\n\n---\n\n<b>Download Full HD Movie with English Subtitles Below\n\n📥 Here 👇\n${botlink}</b>`
+            const photoCaption = `<b>🎬 Movie: ${movieName}</b>\n\n\n<b>📄 Overview:</b>\n${synopsisText}\n\n---\n\n<b>Download Full HD Movie with English Subtitles Below\n\n📥 Download 👇\n${botlink}</b>`
             let tg_res = await downloadFile(durl, socket, fileName, fileCaption, photoCaption, ogImage)
             const {msgid, uniqueId, fileid} = tg_res.telegram
 
