071613364adfdd1e68bd1d3c67f9f4aa3ec4dc96
gshemdoe <georgehmariki@gmail.com>
2025-01-31 14:09:16 +0300

    updated

--- routes/functions/download.js
+++ routes/functions/download.js
@@ -111,7 +111,7 @@ function createUploadProgressStream(filePath, socket) {
     let uploadedSize = 0;
     let lastLogTime = Date.now();
 
-    const readStream = fs.createReadStream(filePath, {highWaterMark: 32*1024}) //32kb;
+    const readStream = fs.createReadStream(filePath, {highWaterMark: 16*1024}) //16kb;
     const passThrough = new PassThrough();
 
     // Calculate bytes read and emit progress every ~1s
